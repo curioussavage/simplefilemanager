5ac65fc7888dd459107277bc9a036af6c3862d57
curioussavage <admin@curioussavage.net>
2020-06-22 12:46:10 -0600

    Functional copy and move

--- src/window.py
+++ src/window.py
@@ -19,6 +19,7 @@ from gi.repository import Gtk
 import os
 import stat
 import mimetypes
+import shutil
 
 from pathlib import Path
 from .file_widget import FileWidget
@@ -28,6 +29,12 @@ mimetypes.init()
 @Gtk.Template(resource_path='/net/curioussavage/simplefilemanager/window.ui')
 class SimplefilemanagerWindow(Gtk.ApplicationWindow):
     __gtype_name__ = 'SimplefilemanagerWindow'
+
+    modify_mode = False
+    modify_type = None
+
+    details_path = None
+    modify_path = None
 
     label = Gtk.Template.Child()
     file_grid = Gtk.Template.Child()
@@ -44,6 +51,13 @@ class SimplefilemanagerWindow(Gtk.ApplicationWindow):
     details_file_type = Gtk.Template.Child()
     details_file_icon = Gtk.Template.Child()
     details_file_size = Gtk.Template.Child()
+    move_btn = Gtk.Template.Child()
+    copy_btn = Gtk.Template.Child()
+    delete_btn = Gtk.Template.Child()
+
+    modify_action_bar = Gtk.Template.Child()
+    cancel_action_btn = Gtk.Template.Child()
+    action_btn = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -52,6 +66,59 @@ class SimplefilemanagerWindow(Gtk.ApplicationWindow):
         self.go_up.connect('clicked', self.go_up_handler)
         self.go_back.connect('clicked', self.go_back_handler)
 
+        self.move_btn.connect('clicked', self.move_handler)
+        self.copy_btn.connect('clicked', self.copy_handler)
+        self.delete_btn.connect('clicked', self.delete_handler)
+
+        self.cancel_action_btn.connect('clicked', self.cancel_action)
+        self.action_btn.connect('clicked', self.do_file_action)
+
+    def enable_modify_mode(self, modify_type):
+        self.modify_mode = True
+        self.modify_type = modify_type
+        self.action_btn.set_label(modify_type + ' here')
+        self.modify_path = self.details_path
+
+        self.modify_action_bar.show()
+
+    def cancel_action(self, w):
+        self.modify_action_bar.hide()
+        self.modify_mode = False
+        self.modify_type = None
+        self.modify_path = None
+
+    def do_file_action(self, w):
+        if self.modify_type == 'copy':
+            if self.modify_path.is_file():
+                try:
+                    shutil.copy2(self.modify_path, self.selected_dir)
+                except Exception:
+                    print('couldn\'t copy file')
+            else:
+                # for directories
+                try:
+                    shutil.copytree(self.modify_path, self.selected_dir)
+                except:
+                    print('couldn\'nt copy dir')
+            print('copied files')
+        elif self.modify_type == 'move':
+            try:
+                shutil.move(self.modify_path, self.selected_dir)
+            except Exception:
+                print('could not move file')
+            print('moved')
+        self.cancel_action(None)
+
+    def move_handler(self, w):
+        self.enable_modify_mode('move')
+        self.main_stack.set_visible_child(self.file_view)
+
+    def copy_handler(self, w):
+        self.enable_modify_mode('copy')
+        self.main_stack.set_visible_child(self.file_view)
+
+    def delete_handler(self, w):
+        pass
 
     def go_back_handler(self, w):
         self.go_back.hide()
@@ -59,6 +126,7 @@ class SimplefilemanagerWindow(Gtk.ApplicationWindow):
         self.main_stack.set_visible_child(self.file_view)
 
     def open_detail_view(self, path):
+        self.details_path = path
         self.go_up.hide()
         stat_res = path.stat()
         self.details_file_name.set_text(path.name)
@@ -66,6 +134,8 @@ class SimplefilemanagerWindow(Gtk.ApplicationWindow):
         if path.is_file():
             ext = path.name.split('.')[1]
             type = mimetypes.types_map.get('.' + ext)
+            if not type:
+                type = 'unknown'
         self.details_file_type.set_text(type)
         self.details_file_size.set_text(str(stat_res.st_size) + ' bytes')
         self.main_stack.set_visible_child(self.detail_view)
@@ -100,7 +170,6 @@ class SimplefilemanagerWindow(Gtk.ApplicationWindow):
 
         for row_num, row in enumerate(sorted_files):
             for file_num, file in enumerate(row):
-                print(file_num)
                 file_widget = FileWidget(file, self)
                 self.file_grid.attach(file_widget, file_num, row_num, 1, 1)
 
