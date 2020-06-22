ffef4b07e60559a66bc023ebbe93f35b0476dd7b
Adam Winn <adam@reddit.com>
2020-06-22 10:48:02 -0600

    Add detail view

--- src/window.py
+++ src/window.py
@@ -17,9 +17,13 @@
 
 from gi.repository import Gtk
 import os
+import stat
+import mimetypes
+
 from pathlib import Path
 from .file_widget import FileWidget
 
+mimetypes.init()
 
 @Gtk.Template(resource_path='/net/curioussavage/simplefilemanager/window.ui')
 class SimplefilemanagerWindow(Gtk.ApplicationWindow):
@@ -30,12 +34,42 @@ class SimplefilemanagerWindow(Gtk.ApplicationWindow):
     header_bar = Gtk.Template.Child()
     go_up = Gtk.Template.Child()
 
+    main_stack = Gtk.Template.Child()
+    file_view = Gtk.Template.Child()
+    detail_view = Gtk.Template.Child()
+
+    #details page
+    go_back = Gtk.Template.Child()
+    details_file_name = Gtk.Template.Child()
+    details_file_type = Gtk.Template.Child()
+    details_file_icon = Gtk.Template.Child()
+    details_file_size = Gtk.Template.Child()
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
         self.change_dir(Path.home())
         self.go_up.connect('clicked', self.go_up_handler)
+        self.go_back.connect('clicked', self.go_back_handler)
+
+
+    def go_back_handler(self, w):
+        self.go_back.hide()
+        self.go_up.show()
+        self.main_stack.set_visible_child(self.file_view)
+
+    def open_detail_view(self, path):
+        self.go_up.hide()
+        stat_res = path.stat()
+        self.details_file_name.set_text(path.name)
+        type = 'folder'
+        if path.is_file():
+            ext = path.name.split('.')[1]
+            type = mimetypes.types_map.get('.' + ext)
+        self.details_file_type.set_text(type)
+        self.details_file_size.set_text(str(stat_res.st_size) + ' bytes')
+        self.main_stack.set_visible_child(self.detail_view)
+        self.go_back.show()
 
     def go_up_handler(self, w):
         self.change_dir(self.selected_dir.parent)
